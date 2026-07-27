cdc79162ae9e89b52d0d8038521c5fcd7eeb5e83
SloshyDev <devlnicanorl@gmail.com>
2026-07-27 01:48:25 -0600

    refactor: align navigation with zinc theme

--- src/components/Header/ui/Link.jsx
+++ src/components/Header/ui/Link.jsx
@@ -5,7 +5,7 @@ const Link = ({ label, page, bgHover, scientificName }) => {
     <li>
       <a
         href={`${page}`}
-        className={`block w-full rounded-md bg-gray-100 px-3 py-1 whitespace-nowrap transition-colors dark:bg-slate-900 dark:text-gray-100 dark:focus-within:bg-slate-700 dark:hover:bg-slate-700 focus-within:${bgHover} hover:${bgHover}`}>
+        className={`block w-full rounded-md bg-zinc-100 px-3 py-1 whitespace-nowrap transition-colors dark:bg-zinc-900 dark:text-zinc-100 dark:focus-within:bg-zinc-700 dark:hover:bg-zinc-700 focus-within:${bgHover} hover:${bgHover}`}>
         {label}
         {scientificName && <span className="text-[#316663] italic dark:text-[#70b7b1]"> ({scientificName})</span>}
       </a>
