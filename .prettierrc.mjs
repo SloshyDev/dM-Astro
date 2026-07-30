5ee941eeb89386ac73a224dc532b491fe9571cad
SloshyDev <devlnicanorl@gmail.com>
2026-07-30 15:37:51 -0600

    chore: configure Astro formatting

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+export default {
+  bracketSameLine:true,
+  printWidth:160,
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};   
