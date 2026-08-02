01ce63939831b9bef6f6c3a861c0e30e3fa95698
SloshyDev <devlnicanorl@gmail.com>
2026-08-02 00:57:09 -0600

    feat: agrega secciones dinámicas en inicio

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ["api.yokaicdmx.com"],
+  },
   build: {
       inlineStylesheets: "always"
   },
